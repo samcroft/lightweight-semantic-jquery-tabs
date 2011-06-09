2f4357a39effcca92f97cfc7c8166bd69491bd8b
Sam Croft <samcroft@gmail.com>
2011-06-09 12:30:59 +0100

    Added support for multiple tab elements on same page.

--- jquery.tabs.js
+++ jquery.tabs.js
@@ -1,16 +1,15 @@
 (function($){
 	$.fn.tabs = function() {
-		var dlHeight;
-		var el;
-		el = this;
-		
-		el.addClass('enhance');
-		dlHeight = el.height();
+		this.each(function() {
+			var el = $(this);
+			el.addClass('enhance');
+			var dlHeight = el.height();
 
-		el.find('dd').hide();
-		var current = el.find('dt:first').addClass('current');
-		var currentHeight = current.next('dd').show().height();
-		el.css('height', dlHeight + currentHeight);
+			el.find('dd').hide();
+			var current = el.find('dt:first').addClass('current');
+			var currentHeight = current.next('dd').show().height();
+			el.css('height', dlHeight + currentHeight);
+		});
 
 		$('dt a').click(function(e){
 			e.preventDefault();
@@ -18,7 +17,9 @@
 			$(this).parents('dl').find('.current').removeClass('current').next('dd').hide();
 			var current = $(this).parent('dt').addClass('current');
 			var currentHeight = current.next('dd').show().height();
-			el.css('height', dlHeight + currentHeight);	
+			var dlHeight = $(this).parents('dl').removeAttr('style').height();
+			
+			$(this).parents('dl').css('height', dlHeight + currentHeight);	
 		});
 	}
 })(jQuery);
