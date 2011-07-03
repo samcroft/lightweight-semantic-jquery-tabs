42f52f87ed064a896fd463b7bc33f81b88a11c48
Sam Croft <samcroft@gmail.com>
2011-07-03 22:23:03 +0100

    Added url hash support to select a specific tab

--- jquery.tabs.js
+++ jquery.tabs.js
@@ -2,14 +2,26 @@
 	$.fn.tabs = function() {
 		this.each(function() {
 			var el = $(this);
+			var current;
+			
 			el.addClass('enhance');
 			var dlHeight = el.height();
 
 			el.find('dd').hide();
-			var current = el.find('dt:first').addClass('current');
+			
+			var hash = location.hash;
+			
+			var hasHash = el.find('a[href="'+hash+'"]');
+			if (hasHash.length) {
+				current = el.find('a[href="'+hash+'"]').parent().addClass('current');
+			} else {
+				current = el.find('dt:first').addClass('current');
+			}
+			
 			var currentHeight = current.next('dd').show().height();
 			el.css('height', dlHeight + currentHeight);
 		});
+
 
 		$('dl.enhance dt a').click(function(e){
 			e.preventDefault();
@@ -19,7 +31,9 @@
 			var currentHeight = current.next('dd').show().height();
 			var dlHeight = $(this).parents('dl').removeAttr('style').height();
 			
-			$(this).parents('dl').css('height', dlHeight + currentHeight);	
+			$(this).parents('dl').css('height', dlHeight + currentHeight);
+			
+			location.hash = $(this).attr('href');
 		});
 	}
 })(jQuery);
